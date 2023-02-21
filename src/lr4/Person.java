39bd1422601a0028fcad72403ac69d56b3d3416d
20404 <20404@1k129s8.dune.edu>
2023-02-21 14:41:52 +0200

    Добавление класса Person

---
+++ src/lr4/Person.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lr4;
+
+/**
+ *
+ * @author 20404
+ */
+public class Person {
+    
+}
